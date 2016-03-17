b4fdc6a9a3d48efbc4e79922a289880feb8e991e
Manuel <manubp9@gmail.com>
2016-03-17 12:26:09 +0100

    modificado para introducir el numero de bolas que quieres crear

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,6 +12,8 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private Random aleatorio;
+    private ArrayList<BouncingBall> listaBolas; 
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -18,12 +21,14 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        aleatorio = new Random();
+        listaBolas = new ArrayList<>();
     }
 
     /**
-     * Simulate two bouncing balls
+     * Simulate a bouncing balls
      */
-    public void bounce()
+    public void bounce(int bolas)
     {
         int ground = 400;   // position of the ground line
 
@@ -31,23 +36,39 @@ public class BallDemo
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
+        int posX = 40;
+        int posY = 40;
+        int radio = 15;
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        for(int i =0;i<bolas;i++)
+        {
+            BouncingBall ball = new BouncingBall(15, posX,posY,Color.BLUE,ground, myCanvas);
+            listaBolas.add(ball);
+            ball.draw();
+            posX=posX+10;
+            posY= posY+10;
+            radio = radio+10;
+        }
 
+        // make them bounce
         // make them bounce
         boolean finished =  false;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            myCanvas.wait(50);
+
+            for(int i = 0 ;i< listaBolas.size();i++)
+            {   
+
+                listaBolas.get(i).move();
+                if(listaBolas.get(i).getXPosition() >= 550) 
+                {
+                    finished = true;
+                }
+
             }
+
+            
         }
+
     }
 }
